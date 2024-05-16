e976e119fee07426357b88656fd934547a364602
nisukmaa <nisukmaa@gmail.com>
2024-05-17 01:05:04 +0700

    Perubahan pada header, footer, pop up message, gradient layer

--- insight.js
+++ insight.js
@@ -10,34 +10,35 @@ document.body.addEventListener("click", (ev) => {
     expandable.classList.toggle("expandable__open");
 });
 
-//sidebar
-document.addEventListener("DOMContentLoaded", function() {
-    const menuButton = document.getElementById("menu-button");
-    const sidebarToggle = document.getElementById("sidebar-toggle");
-  
-    menuButton.addEventListener("click", function() {
-      sidebarToggle.checked = !sidebarToggle.checked;
-    });
-  });
+//pop up
+const messageIcon = document.getElementById('messageIcon');
+const popupContainer = document.getElementById('popupContainer');
+const closePopup = document.getElementById('closePopup');
+const overlay = document.querySelector('.popup-overlay');
 
-const selectBtn = document.querySelector(".select-btn"),
-      items = document.querySelectorAll(".item");
-selectBtn.addEventListener("click", () => {
-    selectBtn.classList.toggle("open");
-});
-items.forEach(item => {
-    item.addEventListener("click", () => {
-        item.classList.toggle("checked");
-    });
-})
-const selectBtn2 = document.querySelector(".select-btn2"),
-      items2 = document.querySelectorAll(".item2");
-selectBtn2.addEventListener("click", () => {
-    selectBtn2.classList.toggle("open");
-});
-items2.forEach(item => {
-    item.addEventListener("click", () => {
-        item.classList.toggle("checked2");
-    });
-})
+let popupOpened = false; // menandakan apakah pop-up sudah pernah dibuka
 
+window.addEventListener('DOMContentLoaded', () => {
+    // mengatur agar popup message tidak muncul saat pertama kali membuka webpage
+    popupContainer.style.display = 'none';
+    overlay.style.display = 'none';
+});
+
+messageIcon.addEventListener('click', () => {
+    if (!popupOpened) {
+        popupContainer.style.display = 'block';
+        overlay.style.display = 'block'; 
+        popupOpened = true;
+    }
+});
+
+closePopup.addEventListener('click', () => {
+    popupContainer.style.display = 'none';
+    overlay.style.display = 'none'; 
+});
+
+// menutup pop-up box jika user mengklik di luar box area
+overlay.addEventListener('click', () => {
+    popupContainer.style.display = 'none';
+    overlay.style.display = 'none';
+});
