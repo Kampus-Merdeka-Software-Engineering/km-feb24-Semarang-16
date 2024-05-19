6042b5cc8095002b5daaaf11f9b12f5ed979132c
barizaarfadillah <arfadillah.abqariy@gmail.com>
2024-05-19 23:21:36 +0700

    update

--- dashboard.js
+++ dashboard.js
@@ -1,12 +1,15 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const menuButton = document.getElementById("menu-button");
-  const sidebarToggle = document.getElementById("sidebar-toggle");
-
-  menuButton.addEventListener("click", function () {
-    sidebarToggle.checked = !sidebarToggle.checked;
+// Sidebar
+document
+  .getElementById("hamburger-menu")
+  .addEventListener("click", function () {
+    document.getElementById("sidebar").classList.add("open");
   });
+
+document.getElementById("menu-button").addEventListener("click", function () {
+  document.getElementById("sidebar").classList.remove("open");
 });
 
+// Filter
 const selectBtn = document.querySelector(".select-btn"),
   items = document.querySelectorAll(".item");
 selectBtn.addEventListener("click", () => {
@@ -29,31 +32,31 @@ items2.forEach((item) => {
 });
 
 //pop up
-document.addEventListener('DOMContentLoaded', () => {
-    const messageIcon = document.getElementById('messageIcon');
-    const popupContainer = document.getElementById('popupContainer');
-    const closePopup = document.getElementById('closePopup');
-    const overlay = document.querySelector('.popup-overlay');
+document.addEventListener("DOMContentLoaded", () => {
+  const messageIcon = document.getElementById("messageIcon");
+  const popupContainer = document.getElementById("popupContainer");
+  const closePopup = document.getElementById("closePopup");
+  const overlay = document.querySelector(".popup-overlay");
 
-    // Mengatur agar popup message tidak muncul saat pertama kali membuka webpage
-    popupContainer.style.display = 'none';
-    overlay.style.display = 'none';
+  // Mengatur agar popup message tidak muncul saat pertama kali membuka webpage
+  popupContainer.style.display = "none";
+  overlay.style.display = "none";
 
-    messageIcon.addEventListener('click', () => {
-        popupContainer.style.display = 'block';
-        overlay.style.display = 'block';
-    });
+  messageIcon.addEventListener("click", () => {
+    popupContainer.style.display = "block";
+    overlay.style.display = "block";
+  });
 
-    closePopup.addEventListener('click', () => {
-        popupContainer.style.display = 'none';
-        overlay.style.display = 'none'; 
-    });
+  closePopup.addEventListener("click", () => {
+    popupContainer.style.display = "none";
+    overlay.style.display = "none";
+  });
 
-    // Menutup pop-up box jika user mengklik di luar box area
-    overlay.addEventListener('click', (event) => {
-        if (event.target === overlay) {
-            popupContainer.style.display = 'none';
-            overlay.style.display = 'none';
-        }
-    });
+  // Menutup pop-up box jika user mengklik di luar box area
+  overlay.addEventListener("click", (event) => {
+    if (event.target === overlay) {
+      popupContainer.style.display = "none";
+      overlay.style.display = "none";
+    }
+  });
 });
