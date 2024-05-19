cca01bf62f882a37ae99f4c25ae7d5e58f8173f1
nisukmaa <nisukmaa@gmail.com>
2024-05-20 02:08:59 +0700

    adjust media query, revisi minor (ini yang bener)

--- home.js
+++ home.js
@@ -1,3 +1,11 @@
+//toggle menu
+const toggleButton = document.getElementById('toggle-button');
+const navLists = document.getElementById('nav-menu-right');
+
+toggleButton.addEventListener('click', () => {
+    navLists.classList.toggle('active');
+})
+
 // slider
 let list = document.querySelector('.slider .list');
 let items = document.querySelectorAll('.slider .list .item');
@@ -31,23 +39,11 @@ setInterval(() => {
 }, 5000); 
 
 //pop up
-document.addEventListener('DOMContentLoaded', () => {
-  const messageIcon = document.getElementById('messageIcon');
-  const popupContainer = document.getElementById('popupContainer');
-  const closePopup = document.getElementById('closePopup');
-  const overlay = document.querySelector('.popup-overlay');
+const messageIcon = document.getElementById('messageIcon');
+const popupContainer = document.getElementById('popupContainer');
+const closePopup = document.getElementById('closePopup');
+const overlay = document.querySelector('.popup-overlay');
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-let popupOpened = false; // menandakan apakah pop-up sudah pernah dibuka
-
->>>>>>> parent of 8b830aa (adjust media query, revisi minor)
-=======
-let popupOpened = false; // menandakan apakah pop-up sudah pernah dibuka
-
->>>>>>> parent of 8b830aa (adjust media query, revisi minor)
 window.addEventListener('DOMContentLoaded', () => {
     // mengatur agar popup message tidak muncul saat pertama kali membuka webpage
     popupContainer.style.display = 'none';
@@ -55,16 +51,14 @@ window.addEventListener('DOMContentLoaded', () => {
 });
 
 messageIcon.addEventListener('click', () => {
-    if (!popupOpened) {
-        popupContainer.style.display = 'block';
-        overlay.style.display = 'block'; 
-        popupOpened = true;
-    }
+    const isVisible = popupContainer.style.display === 'block';
+    popupContainer.style.display = isVisible ? 'none' : 'block';
+    overlay.style.display = isVisible ? 'none' : 'block';
 });
 
 closePopup.addEventListener('click', () => {
     popupContainer.style.display = 'none';
-    overlay.style.display = 'none'; 
+    overlay.style.display = 'none';
 });
 
 // menutup pop-up box jika user mengklik di luar box area
@@ -72,36 +66,6 @@ overlay.addEventListener('click', () => {
     popupContainer.style.display = 'none';
     overlay.style.display = 'none';
 });
-<<<<<<< HEAD
-<<<<<<< HEAD
 
 
 
-=======
-  // Mengatur agar popup message tidak muncul saat pertama kali membuka webpage
-  popupContainer.style.display = 'none';
-  overlay.style.display = 'none';
-
-  messageIcon.addEventListener('click', () => {
-      popupContainer.style.display = 'block';
-      overlay.style.display = 'block';
-  });
-
-  closePopup.addEventListener('click', () => {
-      popupContainer.style.display = 'none';
-      overlay.style.display = 'none'; 
-  });
-
-  // Menutup pop-up box jika user mengklik di luar box area
-  overlay.addEventListener('click', (event) => {
-      if (event.target === overlay) {
-          popupContainer.style.display = 'none';
-          overlay.style.display = 'none';
-      }
-  });
-});
->>>>>>> 6042b5cc8095002b5daaaf11f9b12f5ed979132c
-=======
->>>>>>> parent of 8b830aa (adjust media query, revisi minor)
-=======
->>>>>>> parent of 8b830aa (adjust media query, revisi minor)
--- insight.js
+++ insight.js
@@ -1,3 +1,11 @@
+//toggle menu
+const toggleButton = document.getElementById('toggle-button');
+const navLists = document.getElementById('nav-menu-right');
+
+toggleButton.addEventListener('click', () => {
+    navLists.classList.toggle('active');
+})
+
 //question
 document.body.addEventListener("click", (ev) => {
     const isExpandableTitle = !!ev.target.closest(".expandable__title-bar");
@@ -11,61 +19,30 @@ document.body.addEventListener("click", (ev) => {
 });
 
 //pop up
-document.addEventListener('DOMContentLoaded', () => {
-    const messageIcon = document.getElementById('messageIcon');
-    const popupContainer = document.getElementById('popupContainer');
-    const closePopup = document.getElementById('closePopup');
-    const overlay = document.querySelector('.popup-overlay');
+const messageIcon = document.getElementById('messageIcon');
+const popupContainer = document.getElementById('popupContainer');
+const closePopup = document.getElementById('closePopup');
+const overlay = document.querySelector('.popup-overlay');
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-let popupOpened = false; // menandakan apakah pop-up sudah pernah dibuka
-
->>>>>>> parent of 8b830aa (adjust media query, revisi minor)
-=======
-let popupOpened = false; // menandakan apakah pop-up sudah pernah dibuka
-
->>>>>>> parent of 8b830aa (adjust media query, revisi minor)
 window.addEventListener('DOMContentLoaded', () => {
     // mengatur agar popup message tidak muncul saat pertama kali membuka webpage
-=======
-    // Mengatur agar popup message tidak muncul saat pertama kali membuka webpage
->>>>>>> 6042b5cc8095002b5daaaf11f9b12f5ed979132c
     popupContainer.style.display = 'none';
     overlay.style.display = 'none';
+});
 
-<<<<<<< HEAD
 messageIcon.addEventListener('click', () => {
-    if (!popupOpened) {
-        popupContainer.style.display = 'block';
-        overlay.style.display = 'block'; 
-        popupOpened = true;
-    }
+    const isVisible = popupContainer.style.display === 'block';
+    popupContainer.style.display = isVisible ? 'none' : 'block';
+    overlay.style.display = isVisible ? 'none' : 'block';
 });
 
 closePopup.addEventListener('click', () => {
     popupContainer.style.display = 'none';
-    overlay.style.display = 'none'; 
+    overlay.style.display = 'none';
 });
-=======
-    messageIcon.addEventListener('click', () => {
-        popupContainer.style.display = 'block';
-        overlay.style.display = 'block';
-    });
 
-    closePopup.addEventListener('click', () => {
-        popupContainer.style.display = 'none';
-        overlay.style.display = 'none'; 
-    });
->>>>>>> 6042b5cc8095002b5daaaf11f9b12f5ed979132c
-
-    // Menutup pop-up box jika user mengklik di luar box area
-    overlay.addEventListener('click', (event) => {
-        if (event.target === overlay) {
-            popupContainer.style.display = 'none';
-            overlay.style.display = 'none';
-        }
-    });
+// menutup pop-up box jika user mengklik di luar box area
+overlay.addEventListener('click', () => {
+    popupContainer.style.display = 'none';
+    overlay.style.display = 'none';
 });
