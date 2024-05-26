02bdfedc08331e04535d0da3243d9ddd76e50314
barizaarfadillah <arfadillah.abqariy@gmail.com>
2024-05-26 22:45:21 +0700

    update

--- dashboard.js
+++ dashboard.js
@@ -29,39 +29,39 @@ items2.forEach((item) => {
 });
 
 //pop up
-const messageIcon = document.getElementById('messageIcon');
-const popupContainer = document.getElementById('popupContainer');
-const closePopup = document.getElementById('closePopup');
-const overlay = document.querySelector('.popup-overlay');
-const messageForm = document.getElementById('messageForm');
+const messageIcon = document.getElementById("messageIcon");
+const popupContainer = document.getElementById("popupContainer");
+const closePopup = document.getElementById("closePopup");
+const overlay = document.querySelector(".popup-overlay");
+const messageForm = document.getElementById("messageForm");
 
-window.addEventListener('DOMContentLoaded', () => {
-    // mengatur agar popup message tidak muncul saat pertama kali membuka webpage
-    popupContainer.style.display = 'none';
-    overlay.style.display = 'none';
+window.addEventListener("DOMContentLoaded", () => {
+  // mengatur agar popup message tidak muncul saat pertama kali membuka webpage
+  popupContainer.style.display = "none";
+  overlay.style.display = "none";
 });
 
-messageIcon.addEventListener('click', () => {
-    const isVisible = popupContainer.style.display === 'block';
-    popupContainer.style.display = isVisible ? 'none' : 'block';
-    overlay.style.display = isVisible ? 'none' : 'block';
+messageIcon.addEventListener("click", () => {
+  const isVisible = popupContainer.style.display === "block";
+  popupContainer.style.display = isVisible ? "none" : "block";
+  overlay.style.display = isVisible ? "none" : "block";
 });
 
-closePopup.addEventListener('click', () => {
-    popupContainer.style.display = 'none';
-    overlay.style.display = 'none';
+closePopup.addEventListener("click", () => {
+  popupContainer.style.display = "none";
+  overlay.style.display = "none";
 });
 
 // menutup pop-up box jika user mengklik di luar box area
-overlay.addEventListener('click', () => {
-    popupContainer.style.display = 'none';
-    overlay.style.display = 'none';
+overlay.addEventListener("click", () => {
+  popupContainer.style.display = "none";
+  overlay.style.display = "none";
 });
 
-messageForm.addEventListener('submit', function(event) {
-  event.preventDefault(); 
+messageForm.addEventListener("submit", function (event) {
+  event.preventDefault();
   messageForm.reset();
-  alert('Form submitted successfully!');
+  alert("Form submitted successfully!");
 });
 
 // Manipulasi Data JSON
@@ -149,5 +149,230 @@ document.addEventListener("DOMContentLoaded", function () {
         `;
         listItemsregion.appendChild(listItem);
       });
+
+      // Grafik 1
+      // Fungsi untuk mengonversi angka bulan menjadi nama bulan
+      function getMonthName(month) {
+        const monthNames = [
+          "Jan",
+          "Feb",
+          "Mar",
+          "Apr",
+          "May",
+          "Jun",
+          "Jul",
+          "Aug",
+          "Sep",
+          "Oct",
+          "Nov",
+          "Dec",
+        ];
+        return monthNames[month - 1];
+      }
+
+      // Fungsi untuk mengubah format tanggal ke bulan dan tahun
+      function formatDateToMonthYear(date) {
+        const [day, month, year] = date.split("/");
+        return `${getMonthName(Number(month))} ${year}`;
+      }
+
+      // Mengelompokkan data berdasarkan bulan dan tahun
+      const salesData = {};
+      const profitData = {};
+
+      data.forEach((item) => {
+        const monthYear = formatDateToMonthYear(item["Order Date"]);
+        if (!salesData[monthYear]) {
+          salesData[monthYear] = 0;
+          profitData[monthYear] = 0;
+        }
+        salesData[monthYear] += item.Sales;
+        profitData[monthYear] += item.Profit;
+      });
+
+      // Mengambil bulan/tahun dan mengurutkannya
+      const monthYears = Object.keys(salesData).sort((a, b) => {
+        const [aMonth, aYear] = a.split(" ");
+        const [bMonth, bYear] = b.split(" ");
+        return aYear === bYear ? aMonth.localeCompare(bMonth) : aYear - bYear;
+      });
+
+      // Menyiapkan data untuk chart
+      const sale = monthYears.map((monthYear) => salesData[monthYear]);
+      const profits = monthYears.map((monthYear) => profitData[monthYear]);
+
+      // Membuat chart
+      const ctx = document.getElementById("sales-profit").getContext("2d");
+      const myChart = new Chart(ctx, {
+        type: "line",
+        data: {
+          labels: monthYears,
+          datasets: [
+            {
+              label: "Sales",
+              data: sale,
+              borderColor: "rgba(255, 0, 0, 1)",
+              backgroundColor: "rgba(255, 0, 0, 0.2)",
+              fill: false,
+            },
+            {
+              label: "Total Profit",
+              data: profits,
+              borderColor: "rgba(0, 0, 255, 1)",
+              backgroundColor: "rgba(0, 0, 255, 0.2)",
+              fill: false,
+            },
+          ],
+        },
+        options: {
+          scales: {
+            y: {
+              beginAtZero: true,
+            },
+          },
+        },
+      });
+
+      // Grafik 2
+      var groupedData = {};
+      data.forEach(function (item) {
+        if (!groupedData[item["Region"]]) {
+          groupedData[item["Region"]] = {
+            Region: item["Region"],
+            "Total Sales": item["Sales"],
+            "Total Orders": 1,
+          };
+        } else {
+          groupedData[item["Region"]]["Total Sales"] += item["Sales"];
+          groupedData[item["Region"]]["Total Orders"]++;
+        }
+      });
+
+      // Transforming grouped data into an array
+      var dataArray = [];
+      for (var region in groupedData) {
+        if (groupedData.hasOwnProperty(region)) {
+          var regionData = groupedData[region];
+          // Format total sales to include thousands separator and 1 decimal place
+          var formattedSales = regionData["Total Sales"].toLocaleString(
+            undefined,
+            { minimumFractionDigits: 1, maximumFractionDigits: 1 }
+          );
+          dataArray.push({
+            Region: regionData["Region"],
+            "Total Sales": formattedSales,
+            "Total Orders": regionData["Total Orders"],
+          });
+        }
+      }
+
+      // Initializing DataTables
+      var table = $("#sales-region").DataTable({
+        data: dataArray,
+        columns: [
+          { data: "Region" },
+          { data: "Total Sales" },
+          { data: "Total Orders" },
+        ],
+        order: [[1, "desc"]],
+      });
+
+      // Grafik 3
+      var groupedData = {};
+      data.forEach(function (item) {
+        var key = item["Sub-Category"];
+        if (!groupedData[key]) {
+          groupedData[key] = {
+            Category: item["Category"],
+            "Sub-Category": item["Sub-Category"],
+            "Total Sales": item["Sales"],
+            "Total Quantity": item["Quantity"],
+          };
+        } else {
+          groupedData[key]["Total Sales"] += item["Sales"];
+          groupedData[key]["Total Quantity"] += item["Quantity"];
+        }
+      });
+
+      // Transforming grouped data into an array
+      var dataArray = [];
+      for (var subCategory in groupedData) {
+        if (groupedData.hasOwnProperty(subCategory)) {
+          var subCategoryData = groupedData[subCategory];
+          dataArray.push({
+            Category: subCategoryData["Category"],
+            "Sub-Category": subCategoryData["Sub-Category"],
+            "Total Sales": subCategoryData["Total Sales"].toLocaleString(
+              "en-US",
+              { minimumFractionDigits: 1, maximumFractionDigits: 1 }
+            ),
+            "Total Quantity": subCategoryData["Total Quantity"].toLocaleString(
+              "en-US",
+              { minimumFractionDigits: 1, maximumFractionDigits: 1 }
+            ),
+          });
+        }
+      }
+
+      // Initializing DataTables
+      var table = $("#sales-category").DataTable({
+        data: dataArray,
+        columns: [
+          { data: "Category" },
+          { data: "Sub-Category" },
+          { data: "Total Sales" },
+          { data: "Total Quantity" },
+        ],
+        order: [[2, "desc"]],
+      });
+
+      // Grafik 4
+
+      // Grafik 5
+      const segmentCounts = {};
+      data.forEach((entry) => {
+        const segment = entry.Segment;
+        if (!segmentCounts[segment]) {
+          segmentCounts[segment] = new Set();
+        }
+        segmentCounts[segment].add(entry["Customer ID"]);
+      });
+
+      // Menghitung total jumlah customer untuk setiap segment
+      const segmentLabels = Object.keys(segmentCounts);
+      const segmentData = segmentLabels.map(
+        (segment) => segmentCounts[segment].size
+      );
+
+      // Membuat chart dengan tipe pie
+      const segmentPieCtx = document
+        .getElementById("segmentPieChart")
+        .getContext("2d");
+      const segmentPieChart = new Chart(segmentPieCtx, {
+        type: "pie",
+        data: {
+          labels: segmentLabels,
+          datasets: [
+            {
+              data: segmentData,
+              backgroundColor: [
+                "rgba(183,28,28,255)",
+                "rgba(205,96,96,255)",
+                "rgba(226,164,164,255)",
+              ],
+            },
+          ],
+        },
+        options: {
+          title: {
+            display: true,
+            text: "Customer Segments",
+            fontSize: 18,
+          },
+        },
+      });
+
+      // Grafik 6
+      
     });
 });
